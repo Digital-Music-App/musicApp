82a1be986d9937ffc1702860f9eb01b2e0b4986d
ali maatoug <ali.maatoug@esprit.tn>
2023-10-13 14:14:33 +0100

    hope

--- script.js
+++ script.js
@@ -1,109 +1,120 @@
 
-           // carousel//
-let slideIndex = 1;
+var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
+    showSlides(slideIndex += n);
+    }
+    
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+    let slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {
+        slideIndex = 1
+    }    
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (var i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+    }
+    slides[slideIndex-1].style.display = "block";  
 }
-// carousel//
 
-// - II Music Player//
-// - 1 Music collection
-<<<<<<< HEAD
-// var 
-
-
-
-
-
-var icon = document.getElementById("icon");
-=======
 
 const playButton1 = document.getElementById("icon1");
-    const audioNe3ma1 = document.getElementById("ne3ma1");
-    let isPlaying1 = false;
+const audioNe3ma1 = document.getElementById("ne3ma1");
+var isPlaying1 = false;
 
-    playButton1.addEventListener("click", function () {
-        if (isPlaying1) {
-            audioNe3ma1.pause();
-            playButton1.src = "images/play.png";
-        } else {
-            audioNe3ma1.play();
-            playButton1.src = "images/pause.png"; // Replace with your pause button image
-        }
-        isPlaying1 = !isPlaying1;
-    });
+playButton1.addEventListener("click", function () {
+    if (isPlaying1) {
+        audioNe3ma1.pause();
+        playButton1.src = "images/play.png";
+    } else {
+        audioNe3ma1.play();
+        playButton1.src = "images/pause.png"; 
+    }
+    isPlaying1 = !isPlaying1;
+});
 
 const playButton2 = document.getElementById("icon2");
-    const audioNe3ma2 = document.getElementById("ne3ma2");
-    let isPlaying2 = false;
+const audioNe3ma2 = document.getElementById("ne3ma2");
+let isPlaying2 = false;
 
-    playButton2.addEventListener("click", function () {
-        if (isPlaying2) {
-            audioNe3ma2.pause();
-            playButton2.src = "images/play.png";
-        } else {
-            audioNe3ma2.play();
-            playButton2.src = "images/pause.png"; // Replace with your pause button image
+playButton2.addEventListener("click", function () {
+    if (isPlaying2) {
+        audioNe3ma2.pause();
+        playButton2.src = "images/play.png";
+    } else {
+        audioNe3ma2.play();
+        playButton2.src = "images/pause.png"; 
+    }
+    isPlaying2 = !isPlaying2;
+});
+
+const playButton3 = document.getElementById("icon3");
+const audioJouini1 = document.getElementById("jouini1");
+let isPlaying3 = false;
+
+playButton3.addEventListener("click", function () {
+    if (isPlaying3) {
+        audioJouini1.pause();
+        playButton3.src = "images/play.png";
+    } else {
+        audioJouini1.play();
+        playButton3.src = "images/pause.png"; 
+    }
+    isPlaying3 = !isPlaying3;
+});
+
+const playButton4 = document.getElementById("icon4");
+const audioJouini2 = document.getElementById("jouini2");
+let isPlaying4 = false;
+
+playButton4.addEventListener("click", function () {
+    if (isPlaying4) {
+        audioJouini2.pause();
+        playButton4.src = "images/play.png";
+    } else {
+        audioJouini2.play();
+        playButton4.src = "images/pause.png"; 
+    }
+    isPlaying4 = !isPlaying4;
+});
+
+    
+    
+const searchInput = document.getElementById('searchInput');
+const searchResults = document.getElementById('searchResults');
+
+const items = [];
+var songsName = document.getElementsByClassName("text")
+        for (var i =0 ; i < songsName.length ; i++){
+            items.push(songsName[i].innerHTML)
         }
-        isPlaying2 = !isPlaying2;
+
+// Function to filter and display search results
+function updateSearchResults() {
+
+    const searchTerm = searchInput.value.toLowerCase();
+    const filteredItems = items.filter(item => item.toLowerCase().includes(searchTerm));
+
+    // Clear previous results
+    searchResults.innerHTML = '';
+
+    // Display filtered results
+    filteredItems.forEach(item => {
+    const listItem = document.createElement('li');
+    listItem.textContent = item;
+    searchResults.appendChild(listItem);
     });
+}
 
-  const playButton3 = document.getElementById("icon3");
-    const audioJouini1 = document.getElementById("jouini1");
-    let isPlaying3 = false;
+function clearInput(){
+if (searchInput.value.length === 0)
+searchResults.innerHTML = '';
+}
 
-    playButton3.addEventListener("click", function () {
-        if (isPlaying3) {
-            audioJouini1.pause();
-            playButton3.src = "images/play.png";
-        } else {
-            audioJouini1.play();
-            playButton3.src = "images/pause.png"; // Replace with your pause button image
-        }
-        isPlaying3 = !isPlaying3;
-    });
+searchInput.addEventListener("keyup",clearInput);
 
-    const playButton4 = document.getElementById("icon4");
-    const audioJouini2 = document.getElementById("jouini2");
-    let isPlaying4 = false;
-
-    playButton4.addEventListener("click", function () {
-        if (isPlaying4) {
-            audioJouini2.pause();
-            playButton4.src = "images/play.png";
-        } else {
-            audioJouini2.play();
-            playButton4.src = "images/pause.png"; // Replace with your pause button image
-        }
-        isPlaying4 = !isPlaying4;
-    });
-
-
-//     var aze = document.getElementsByClassName("text")
-
-// for (var i =0 ; i < aze.length ; i++){
-//   console.log(aze[i].innerHTML);
-// }
->>>>>>> b98a65eae06564711d5812e4a700d979b06b67b9
+// Event listener for input changes
+searchInput.addEventListener('input', updateSearchResults);
