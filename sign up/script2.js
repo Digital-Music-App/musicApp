5e4d07a3aa2024b536a19342261a50ba0bcc0c06
Khalil420777 <bouarroujkhalil@gmail.com>
2023-10-13 16:12:14 +0100

    khalil

--- sign up/script2.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('loginButton').addEventListener('click', function(event) {
-    // Prevent the default form submission behavior
-    event.preventDefault();
-    
-    // Redirect the user to the desired URL
-    window.location.href = 'file:///C:/Users/khalil/Desktop/musicApp/index.html#';
-});
